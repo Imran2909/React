00be1d195cfba58261563ff8344ac305db0e4164
Imran2909 <sutarimran47@gmail.com>
2024-05-02 18:54:18 +0530

    test

---
+++ 469-514-Jpwgjy/masai-ops-469-514-Jpwgjy-6b16e58687033674e91d11aa0f78152a275c4bfe/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import MobileOS from "./components/MobileOS";
+
+function App() {
+  return <div>{
+    /* add MObileOs component here */
+    <MobileOS />
+    }</div>;
+    //https://github.com/masai-course/arnab_adhikary_fw22_0574/tree/master/unit-4/sprint-1/day-2/assignments/masai-ops-469-514-Jpwgjy-6b16e58687033674e91d11aa0f78152a275c4bfe
+
+}
+
+export default App;
---
+++ 469-514-Jpwgjy/masai-ops-469-514-Jpwgjy-6b16e58687033674e91d11aa0f78152a275c4bfe/src/components/MobileOS.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MobileOS() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MobileOS
